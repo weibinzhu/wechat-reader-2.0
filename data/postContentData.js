e98e38de446ece4d1ed930f6eab9f3e60fd9bc9c
weibinzhu <fatbin37@hotmail.com>
2017-06-21 22:47:39 +0800

    add data

---
+++ data/postContentData.js
@@ -0,0 +1,44 @@
+var localDatabase = [
+  {
+    postAuthorImg: "/images/avatar.png",
+    postDate: "JUN 2 2017",
+    postTitle: "这个杀手不太冷",
+    postImg: "/images/artical1.jpg",
+    postContent: "叙事节奏清晰流畅，毫不拖泥带水；Léon、Stansfield、Mathilda，几个主要人物个性鲜明； 加上如同MTV般快速切换的画面，影片从头到尾紧抓观众的眼球，非常难得。更因为此片，让全世界认识到了娜塔莉·波特曼。",
+    postLikeNum: 15,
+    postCommentNum: 59,
+    headImg: "/images/artical1.jpg",
+    authorName: "林白衣",
+    postTime: "12小时前",
+    detailArtical: "数据绑定一个常见需求是操作元素的 class 列表和它的内联样式。因为它们都是属性 ，我们可以用v-bind 处理它们：只需要计算出表达式最终的字符串。不过，字符串拼接麻烦又易错。因此，在 v-bind 用于 class 和 style 时， Vue.js 专门增强了它。表达式的结果类型除了字符串之外，还可以是对象或数组。HTML 标准中块级元素和行内元素的区别至高出现在 4.01 标准中。在 HTML5，这种区别被一个更复杂的内容类别代替。”块级“类别大致相当于 HTML5 中的流内容类别，而”行内“类别相当于 HTML5 中的措辞内容类别，不过除了这两个还有其他类别。数据绑定一个常见需求是操作元素的 class 列表和它的内联样式。因为它们都是属性 ，我们可以用v-bind 处理它们：只需要计算出表达式最终的字符串。不过，字符串拼接麻烦又易错。因此，在 v-bind 用于 class 和 style 时， Vue.js 专门增强了它。表达式的结果类型除了字符串之外，还可以是对象或数组。HTML 标准中块级元素和行内元素的区别至高出现在 4.01 标准中。在 HTML5，这种区别被一个更复杂的内容类别代替。”块级“类别大致相当于 HTML5 中的流内容类别，而”行内“类别相当于 HTML5 中的措辞内容类别，不过除了这两个还有其他类别。数据绑定一个常见需求是操作元素的 class 列表和它的内联样式。因为它们都是属性 ，我们可以用v-bind 处理它们：只需要计算出表达式最终的字符串。不过，字符串拼接麻烦又易错。因此，在 v-bind 用于 class 和 style 时， Vue.js 专门增强了它。表达式的结果类型除了字符串之外，还可以是对象或数组。HTML 标准中块级元素和行内元素的区别至高出现在 4.01 标准中。在 HTML5，这种区别被一个更复杂的内容类别代替。”块级“类别大致相当于 HTML5 中的流内容类别，而”行内“类别相当于 HTML5 中的措辞内容类别，不过除了这两个还有其他类别。数据绑定一个常见需求是操作元素的 class 列表和它的内联样式。因为它们都是属性 ，我们可以用v-bind 处理它们：只需要计算出表达式最终的字符串。不过，字符串拼接麻烦又易错。因此，在 v-bind 用于 class 和 style 时， Vue.js 专门增强了它。表达式的结果类型除了字符串之外，还可以是对象或数组。HTML 标准中块级元素和行内元素的区别至高出现在 4.01 标准中。在 HTML5，这种区别被一个更复杂的内容类别代替。”块级“类别大致相当于 HTML5 中的流内容类别，而”行内“类别相当于 HTML5 中的措辞内容类别，不过除了这两个还有其他类别。数据绑定一个常见需求是操作元素的 class 列表和它的内联样式。因为它们都是属性 ，我们可以用v-bind 处理它们：只需要计算出表达式最终的字符串。不过，字符串拼接麻烦又易错。因此，在 v-bind 用于 class 和 style 时， Vue.js 专门增强了它。表达式的结果类型除了字符串之外，还可以是对象或数组。HTML 标准中块级元素和行内元素的区别至高出现在 4.01 标准中。在 HTML5，这种区别被一个更复杂的内容类别代替。”块级“类别大致相当于 HTML5 中的流内容类别，而”行内“类别相当于 HTML5 中的措辞内容类别，不过除了这两个还有其他类别。数据绑定一个常见需求是操作元素的 class 列表和它的内联样式。因为它们都是属性 ，我们可以用v-bind 处理它们：只需要计算出表达式最终的字符串。不过，字符串拼接麻烦又易错。因此，在 v-bind 用于 class 和 style 时， Vue.js 专门增强了它。表达式的结果类型除了字符串之外，还可以是对象或数组。HTML 标准中块级元素和行内元素的区别至高出现在 4.01 标准中。在 HTML5，这种区别被一个更复杂的内容类别代替。”块级“类别大致相当于 HTML5 中的流内容类别，而”行内“类别相当于 HTML5 中的措辞内容类别，不过除了这两个还有其他类别。",
+    postId: 0,
+    music: {
+      url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+      title: "示例音乐1",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000"
+    }
+  },
+  {
+    postAuthorImg: "/images/avatar2.jpg",
+    postDate: "JUN 2 2017",
+    postTitle: "这个杀手不太冷",
+    postImg: "/images/swiper1.jpg",
+    postContent: "叙事节奏清晰流畅，毫不拖泥带水；Léon、Stansfield、Mathilda，几个主要人物个性鲜明； 加上如同MTV般快速切换的画面，影片从头到尾紧抓观众的眼球，非常难得。更因为此片，让全世界认识到了娜塔莉·波特曼。",
+    postLikeNum: 12,
+    postCommentNum: 6,
+    headImg: "/images/artical1.jpg",
+    authorName: "kast",
+    postTime: "12小时前",
+    detailArtical: "人们总是把自己的情感投射到动物身上。海豚如此受喜爱，大约是因为它们微笑的样子吧。6000 多万年前开始的演化，将这位微笑的朋友带入了海里，水生哺乳动物的先祖自此与陆上的我们走上了完全不同的生存道路。实际上，鲸、海豚和偶蹄目动物共享一个先祖，不知什么原因——古生物学家认为大概是为了觅食——它们中的一些开始进入海滨的浅水中。自然选择的力量如此强大，这些动物们的脚上先是长出了蹼，后来脚也逐渐退化，前肢成为了鳍一样的结构，后肢则几乎没了影踪。毛发基本消失不见，皮肤变得光滑，鼻孔也移到了头顶，一切都是为了游泳专家这个设定。（p.S.留在陆地上的那一支，成为了河马……）",
+    postId: 1,
+    music: {
+      url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+      title: "示例音乐2",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+    }
+  }
+]
+
+module.exports = {
+  postList: localDatabase
+}
